fa9e00578c1ad82d17d9a6a3c9f8ae1fba887335
ZasKbM <eduardbm999@gmail.com>
2018-12-16 03:48:57 +0100

    Added an odd finder

--- scripts/main.js
+++ scripts/main.js
@@ -13,6 +13,7 @@ function main() {
 
   const getodds = async p =>{
     cuotaVal = [cuotas[0].value, cuotas[1].value ,cuotas[2].value];
+    validas =[1,1,1];
     for (let i = 0; i<3; i++){
         if(cuotaVal[i]!=0){
           perdidas = perdidas + (1/cuotaVal[i]);
@@ -35,32 +36,64 @@ function main() {
   const updateStakes = async t => {
     let usable = [];
     for(let i=0; i<3; i++){
-      if(validas[i]) usable.push(stakes[i].value/((1/cuotaVal[i])/perdidas)) ;
+      if(validas[i]) usable.push(stakes[i].value/((1/cuotaVal[i])/perdidas));
     }
   usable.sort();
     let total = usable[0];
+    if (total == null) total =100;
     for(let i=0; i<3; i++){
-      stakes[i].value = (total*((1/cuotaVal[i])/perdidas)).toFixed(2);
+      if (cuotaVal[i]!=0) stakes[i].value = (total/(cuotaVal[i]*perdidas)).toFixed(2);
     }
     console.log(total);
+  }
+
+  const findOdds = async t => {
+    let total =0;
+    for(let i=0; i<3; i++){
+      total += parseInt(stakes[i].value);
+    }
+    for(let i=0; i<3; i++){
+      if(stakes[i].value !=0)cuotas[i].value= (total/stakes[i].value).toFixed(2);
+    }
   }
 
   const calcular = async p => {
     getodds();
     perdida();
-    updateStakes();
     if(perdidas > 1.05){
-    texto.innerHTML = `
-      <div id="perdido">
-        <p>Esta operación dará ${(1/perdidas).toFixed(2)} euros por euro apostado.</p>
-      </div>`
-    ;}
-    else {
+      updateStakes();
+      texto.innerHTML = `
+        <div id="perdido">
+          <p>Esta operación dará ${(1/perdidas).toFixed(2)} euros por euro apostado.</p>
+        </div>`
+      ;}
+    else if (perdidas > 0){
+      updateStakes();
       texto.innerHTML = `
         <div id="ganado">
           <p>Esta operación dará ${(1/perdidas).toFixed(2)} euros por euro apostado.</p>
         </div>`
       ;
+    }
+    else {
+      findOdds();
+      getodds();
+      perdida();
+      if(perdidas > 1.05){
+        updateStakes();
+        texto.innerHTML = `
+          <div id="perdido">
+            <p>Esta operación dará ${(1/perdidas).toFixed(2)} euros por euro apostado.</p>
+          </div>`
+        ;}
+      else {
+        updateStakes();
+        texto.innerHTML = `
+          <div id="ganado">
+            <p>Esta operación dará ${(1/perdidas).toFixed(2)} euros por euro apostado.</p>
+          </div>`
+        ;
+      }
     }
     for (let a = 0; a<3; a++ ){
       ganancias[a].innerHTML =(cuotaVal[a]*stakes[a].value).toFixed(2);
